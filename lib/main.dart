5d328d8584028ac05ad5961d482cd99b1f064382
FlutLab.io <>
2022-08-16 22:39:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: const <Widget>[
-          Text(
-            'Primeiros passos em flutter',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Inicio de semestre',
-            textDirection: TextDirection.ltr,
-          ),
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.contain, // otherwise the logo will be tiny
-              child: FlutterLogo(),
-            ),
-          ),
-        ],
-      ),
-    );
+void main() => runApp(MaterialApp(
+  home: Text('hello word'),
+),
+);
