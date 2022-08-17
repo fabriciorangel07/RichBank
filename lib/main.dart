63da3db201bca89b54d54b71f64c4e4b0fdfb4c8
FlutLab.io <>
2022-08-17 00:04:48 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,29 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          body: Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('123456-7'),
-              subtitle: Text('1000'),
+          body: Column(children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('123456-7'),
+                subtitle: Text('1000'),
+              ),
             ),
-          ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('123456-7'),
+                subtitle: Text('1000'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('123456-7'),
+                subtitle: Text('1000'),
+              ),
+            ),
+          ]),
           appBar: AppBar(
             title: Text('Transferência'),
           ),
