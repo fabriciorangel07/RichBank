1baceef4f3d83d8910966ac4781cec6b00c6572c
FlutLab.io <>
2022-08-17 00:37:09 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -24,27 +24,21 @@ class ListaTransferencia extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Column(children: [
-      Card(
-        child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text('123456-7'),
-          subtitle: Text('1000'),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text('123456-7'),
-          subtitle: Text('1000'),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text('123456-7'),
-          subtitle: Text('1000'),
-        ),
-      ),
+      ItemTransferencia(),
     ]);
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text('123456-7'),
+        subtitle: Text('1000'),
+      ),
+    );
   }
 }
