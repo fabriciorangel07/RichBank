e38e270f54e97cc491a579cd0893ee7c625cfbe5
FlutLab.io <>
2022-09-13 23:54:26 +0000

    att

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,17 @@
-import 'package:flutter/cupertino.dart';
+import 'package:aula01/screens/transferencias/lista_transferencia.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: ListaTransferencia(),
-          appBar: AppBar(
-            title: Text('Transferência'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              // Add your onPressed code here!
-            },
-            backgroundColor: Colors.blue,
-            child: const Icon(Icons.add),
-          ),
-        ),
-      ),
+      Banco(),
     );
 
-class ListaTransferencia extends StatelessWidget {
+class Banco extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(children: [
-      ItemTransferencia('1.000,00', '1234-5'),
-      ItemTransferencia('2.000,00', '1234-5'),
-    ]);
-  }
-}
-
-class ItemTransferencia extends StatelessWidget {
-  final String valor;
-  final String numeroConta;
-
-  ItemTransferencia(this.valor, this.numeroConta);
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Card(
-      child: ListTile(
-        leading: Icon(Icons.monetization_on),
-        title: Text(valor),
-        subtitle: Text(numeroConta),
+    return MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencia(),
       ),
     );
   }
